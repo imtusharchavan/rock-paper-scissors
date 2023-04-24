e9f90df574173a6c4b62c812bd1c7c54281b42c0
Tushar Chavan <tusharrchavan014@gmail.com>
2023-04-24 16:06:21 +0530

    Play a single round

--- main.js
+++ main.js
@@ -4,4 +4,28 @@ function getComputerChoice() {
   return choices[randomChoice];
 }
 
-console.log(getComputerChoice());
+
+playerSelection = getComputerChoice(); 
+computerSelection = getComputerChoice();
+
+
+function singleRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Tie!";
+  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    return "You Lose! Scissors beats Paper";
+  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    return "You Lose! Rock beats Scissors";
+  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    return "You Won! Rock beats Scissors";
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    return "You Won! Scissors beats Paper";
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    return "You Won! Paper beats Rock";
+  }
+}
+
+
+console.log(singleRound(playerSelection, computerSelection));
